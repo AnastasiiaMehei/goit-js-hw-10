f70d779428a283751a74a4e9fddb9c95baa42e3b
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-14 17:18:33 +0200

    1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,22 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-let userSelectedDate = 0;
-flatpickr('.input#datetime-picker', { options });
+const input = document.getElementById('datetime-picker');
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  button.setAttribute('disabled', '');
+});
+let userSelectedDate = null;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
     console.log(selectedDates[0]);
   },
 };
-const dateStart = new Data('2023-10-081T7:25');
-const dateEnd = new Data();
+flatpickr(input, options);
+
+// const dateStart = new Data('2023-10-081T7:25');
+// const dateEnd = new Data();
