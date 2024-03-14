4878a73516436059f3d2abffe4d6fb25aec07c4c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-14 17:25:14 +0200

    1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,6 +4,7 @@ const input = document.getElementById('datetime-picker');
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
   button.setAttribute('disabled', '');
+  return window.alert('Please choose a date in the future');
 });
 let userSelectedDate = null;
 const options = {
@@ -18,5 +19,25 @@ const options = {
 };
 flatpickr(input, options);
 
-// const dateStart = new Data('2023-10-081T7:25');
-// const dateEnd = new Data();
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
