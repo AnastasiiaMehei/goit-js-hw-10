30a2fdabe1f132d577cc86fcae6921cd37597b3c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-15 20:50:37 +0200

    1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,27 +1,36 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-const form = document.querySelector('form');
-const delayInput = parseInt(
-  document.querySelector("input[name='delay']".value)
-);
-const stateInput = document.querySelector("input[name='state']:checked");
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  const delay = parseInt(delayInput);
+const delayForm = document.querySelector('.form');
+delayForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const timer = event.currentTarget.elements.delay.value;
+  const radio = event.currentTarget.elements.state.value;
+
   const promise = new Promise((resolve, reject) => {
-    if (stateInput.value == 'fulfilled') {
-      setTimeout(() => {
-        resolve(delay);
-      }, delay);
-    } else {
-      reject(delay);
-    }
+    setTimeout(() => {
+      console.log(timer, radio);
+      if (radio === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, timer);
   });
+
+  // Registering promise callbacks
   promise
-    .then(delay => {
-      console.log(`✅ Fulfilled promise in ${delay}ms`);
+    .then(value => {
+      iziToast.success({
+        color: 'green',
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${timer}ms`,
+      });
     })
-    .catch(delay => {
-      console.log(`❌ Rejected promise in ${delay}ms`);
+    .catch(error => {
+      iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: `❌ Rejected promise in ${timer}ms`,
+      });
     });
 });
