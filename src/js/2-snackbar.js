8ca956eb0bbfa29ab754721ab518f5dd79e4fd5c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-15 19:35:02 +0200

    1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,7 +2,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('submit');
-submitBtn.addEventListener('submit', () => {
+form.addEventListener('submit', () => {
   preventDefault();
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
