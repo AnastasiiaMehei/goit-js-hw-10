1ea6fc09e7527e432fb17525516b8b57e5f5944d
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-15 19:50:02 +0200

    111

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,17 +1,27 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 const form = document.querySelector('form');
-const submitBtn = document.querySelector('submit');
+const delayInput = parseInt(
+  document.querySelector("input[name='delay']".value)
+);
+const steteInput = document.querySelector("input[name='state']:checked");
 form.addEventListener('submit', evt => {
   evt.preventDefault();
+  const delay = parseInt(delayInput);
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (isSuccess) {
-        resolve(`✅ Fulfilled promise in ${delay}ms`);
+        resolve(delay);
       } else {
-        reject(`❌ Rejected promise in ${delay}ms`);
+        reject(delay);
       }
     });
-  }, delay);
-  return promise;
+  });
+  promise
+    .then(delay => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+    })
+    .catch(delay => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+    });
 });
