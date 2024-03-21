a5a63a6e642bf71f9bff2ab542e53d100f50c9e9
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-21 14:20:21 +0200

    1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,35 +1,71 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-const delayForm = document.querySelector('.form');
-delayForm.addEventListener('submit', event => {
-  event.preventDefault();
-  const timer = Number(event.currentTarget.elements.delay.value);
-  const radio = event.currentTarget.elements.state.value;
-
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const inputDelay = parseInt(
+    document.querySelector('input[name="delay"]').value
+  );
+  const selectedCheckbox = document.querySelector(
+    'input[name="state"]:checked'
+  ).value;
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (radio === 'fulfilled') {
-        resolve(`✅Fulfilled promise in ${timer}ms`);
+      if (selectedCheckbox === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${inputDelay}ms`);
       } else {
-        reject(`❌Rejected promise in ${timer}ms`);
+        reject(`❌ Rejected promise in ${inputDelay}ms`);
       }
-    }, timer);
+    }, inputDelay);
   });
-
-  // Registering promise callbacks
-  promise
-    .then(value => {
+  promise.then(
+    value => {
       iziToast.success({
         backgroundColor: 'green',
         position: 'topRight',
-        message: `✅ Fulfilled promise in ${timer}ms`,
+        message: `✅ Fulfilled promise in ${inputDelay}ms`,
       });
-    })
-    .catch(error => {
+    },
+    error => {
       iziToast.error({
         backgroundColor: 'red',
         position: 'topRight',
-        message: `❌ Rejected promise in ${timer}ms`,
+        message: `❌ Rejected promise in ${inputDelay}ms`,
       });
-    });
+    }
+  );
 });
+
+// const delayForm = document.querySelector('.form');
+// delayForm.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const timer = Number(event.currentTarget.elements.delay.value);
+//   const radio = event.currentTarget.elements.state.value;
+
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (radio === 'fulfilled') {
+//         resolve(`✅Fulfilled promise in ${timer}ms`);
+//       } else {
+//         reject(`❌Rejected promise in ${timer}ms`);
+//       }
+//     }, timer);
+//   });
+
+//   // Registering promise callbacks
+//   promise
+//     .then(value => {
+//       iziToast.success({
+//         backgroundColor: 'green',
+//         position: 'topRight',
+//         message: `✅ Fulfilled promise in ${timer}ms`,
+//       });
+//     })
+//     .catch(error => {
+//       iziToast.error({
+//         backgroundColor: 'red',
+//         position: 'topRight',
+//         message: `❌ Rejected promise in ${timer}ms`,
+//       });
+//     });
+// });
