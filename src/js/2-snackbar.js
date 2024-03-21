b642a815234f23bf51bc7919ad034e9562dd06a7
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-21 15:44:28 +0200

    1223

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -37,37 +37,3 @@ form.addEventListener('submit', e => {
     }
   );
 });
-
-// const delayForm = document.querySelector('.form');
-// delayForm.addEventListener('submit', event => {
-//   event.preventDefault();
-//   const timer = Number(event.currentTarget.elements.delay.value);
-//   const radio = event.currentTarget.elements.state.value;
-
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (radio === 'fulfilled') {
-//         resolve(`✅Fulfilled promise in ${timer}ms`);
-//       } else {
-//         reject(`❌Rejected promise in ${timer}ms`);
-//       }
-//     }, timer);
-//   });
-
-//   // Registering promise callbacks
-//   promise
-//     .then(value => {
-//       iziToast.success({
-//         backgroundColor: 'green',
-//         position: 'topRight',
-//         message: `✅ Fulfilled promise in ${timer}ms`,
-//       });
-//     })
-//     .catch(error => {
-//       iziToast.error({
-//         backgroundColor: 'red',
-//         position: 'topRight',
-//         message: `❌ Rejected promise in ${timer}ms`,
-//       });
-//     });
-// });
